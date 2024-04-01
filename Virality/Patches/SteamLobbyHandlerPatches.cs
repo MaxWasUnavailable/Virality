6d1906b3130ba6f27017d6f0237b886146fa6cb5
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-04-02 00:38:33 +0200

    Target HostMatch instead

--- Virality/Patches/SteamLobbyHandlerPatches.cs
+++ Virality/Patches/SteamLobbyHandlerPatches.cs
@@ -7,13 +7,13 @@ namespace Virality.Patches;
 internal static class SteamLobbyHandlerPatches
 {
     /// <summary>
-    ///     Postfix patch for the Awake method.
+    ///     Prefix patch for the HostMatch method.
     ///     Overrides the max players value with the one from the Virality config.
     /// </summary>
     /// <param name="__instance"> Instance of the SteamLobbyHandler. </param>
-    [HarmonyPostfix]
-    [HarmonyPatch("Awake")]
-    private static void AwakePostfix(ref SteamLobbyHandler __instance)
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(SteamLobbyHandler.HostMatch))]
+    private static void HostMatchPrefix(ref SteamLobbyHandler __instance)
     {
         __instance.m_MaxPlayers = Virality.MaxPlayers!.Value;
     }
