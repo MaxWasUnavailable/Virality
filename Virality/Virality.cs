9fd70cd8ce8ee15cf7859634c6e183499988e9f1
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-04-02 03:46:08 +0200

    Add config values

--- Virality/Virality.cs
+++ Virality/Virality.cs
@@ -15,6 +15,8 @@ public class Virality : BaseUnityPlugin
     private Harmony? Harmony { get; set; }
     internal new static ManualLogSource? Logger { get; private set; }
     internal static ConfigEntry<int>? MaxPlayers { get; private set; }
+    internal static ConfigEntry<bool>? AllowFriendJoining { get; private set; }
+    internal static ConfigEntry<bool>? AllowLateJoin { get; private set; }
 
     /// <summary>
     ///     Singleton instance of the plugin.
@@ -32,6 +34,12 @@ public class Virality : BaseUnityPlugin
         // Init config entries
         MaxPlayers = Config.Bind("General", "MaxPlayers", 10,
             "The maximum number of players allowed in your lobby.");
+        
+        AllowFriendJoining = Config.Bind("General", "AllowFriendJoining", true, 
+            "Whether or not to allow friends to join your lobby through the Steam overlay.");
+        
+        AllowLateJoin = Config.Bind("General", "AllowLateJoin", true,
+            "Whether or not to allow players to join your lobby after the game has started.");
 
         // Patch using Harmony
         PatchAll();
