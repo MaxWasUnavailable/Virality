a7cde16b4a8ac9523a6a420911cc7087fb40926d
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-04-02 06:42:50 +0200

    Add docstrings

--- Virality/Patches/PhotonGameLobbyHandlerPatches.cs
+++ Virality/Patches/PhotonGameLobbyHandlerPatches.cs
@@ -9,39 +9,45 @@ namespace Virality.Patches;
 [HarmonyPriority(Priority.First)]
 internal static class PhotonGameLobbyHandlerPatches
 {
+    /// <summary>
+    ///     Hide the lobby when the PhotonGameLobbyHandler is started and we're not on the surface.
+    /// </summary>
     [HarmonyPostfix]
     [HarmonyPatch(nameof(PhotonGameLobbyHandler.Start))]
     private static void StartPostfixHide()
     {
         if (!Virality.AllowLateJoin!.Value)
             return;
-        
+
         if (PhotonLobbyHelper.IsOnSurface())
             return;
-        
+
         Virality.Logger?.LogDebug("Hiding lobby.");
-        
+
         SteamLobbyHelper.LobbyHandler?.HideLobby();
         PhotonNetwork.CurrentRoom.IsOpen = false;
         PhotonNetwork.CurrentRoom.IsVisible = false;
         SteamFriends.ClearRichPresence();
     }
-    
+
+    /// <summary>
+    ///     Enable late joining when the PhotonGameLobbyHandler is started and we're on the surface.
+    /// </summary>
     [HarmonyPostfix]
     [HarmonyPatch(nameof(PhotonGameLobbyHandler.Start))]
     private static void StartPostfixOpen()
     {
         if (!Virality.AllowLateJoin!.Value)
             return;
-        
+
         if (!PhotonLobbyHelper.IsOnSurface())
             return;
-        
+
         if (!PhotonNetwork.CurrentRoom.IsOpen && !PhotonNetwork.CurrentRoom.IsVisible)
             return;
-        
+
         Virality.Logger?.LogDebug("Showing lobby.");
-        
+
         SteamLobbyHelper.LobbyHandler?.OpenLobby();
         PhotonNetwork.CurrentRoom.IsOpen = true;
         PhotonNetwork.CurrentRoom.IsVisible = true;
