f3b279aa525ba60d7ad46339bdd313ef7090fd8d
wwwDayDream <daymonforeman14@gmail.com>
2024-04-01 19:54:45 -0500

    Fixes the theoretical sleeping issue.

---
+++ Virality/Patches/AllPlayersAsleep.cs
@@ -0,0 +1,21 @@
+using HarmonyLib;
+
+namespace Virality.Patches;
+
+[HarmonyPatch(typeof(PlayerHandler))]
+[HarmonyPriority(Priority.First)]
+internal static class AllPlayersAsleep {
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(PlayerHandler.AllPlayersAsleep))]
+    private static void AllPlayersInBedPostfix(PlayerHandler __instance, ref bool __result)
+    {
+        var numberAsleep = 0;
+        for (var i = 0; i < __instance.playerAlive.Count; i++)
+        {
+            if (__instance.playerAlive[i].data.sleepAmount >= 0.9f)
+                numberAsleep++;
+        }
+
+        __result = numberAsleep >= 4;
+    }
+}
