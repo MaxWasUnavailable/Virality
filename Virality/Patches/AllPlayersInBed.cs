62e3d6ba5fe04b6c6a03814a6eff5c23bbd18e39
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-04-02 07:53:37 +0200

    Add docstrings

--- Virality/Patches/AllPlayersInBed.cs
+++ Virality/Patches/AllPlayersInBed.cs
@@ -5,17 +5,21 @@ namespace Virality.Patches;
 
 [HarmonyPatch(typeof(PlayerHandler))]
 [HarmonyPriority(Priority.First)]
-internal static class AllPlayersInBed {
+internal static class AllPlayersInBed
+{
+    /// <summary>
+    ///     Changes check for players being in bed to require at most 4 players to be in bed.
+    /// </summary>
+    /// <param name="__instance"> The PlayerHandler instance. </param>
+    /// <param name="__result"> The original result. </param>
     [HarmonyPostfix]
     [HarmonyPatch(nameof(PlayerHandler.AllPlayersInBed))]
     private static void AllPlayersInBedPostfix(PlayerHandler __instance, ref bool __result)
     {
         var numberInBed = 0;
         for (var i = 0; i < __instance.playerAlive.Count; i++)
-        {
             if (__instance.playerAlive[i].data.bed != null)
                 numberInBed++;
-        }
 
         __result = numberInBed >= Math.Min(__instance.playerAlive.Count, 4);
     }
