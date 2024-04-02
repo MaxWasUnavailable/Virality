98979cffbc67f87610305aa310f37351f154fe66
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-04-02 02:08:08 +0200

    Create SteamLobbyHelper

---
+++ Virality/Helpers/SteamLobbyHelper.cs
@@ -0,0 +1,12 @@
+namespace Virality.Helpers;
+
+/// <summary>
+///     Helper class for Steam lobby functionality.
+/// </summary>
+public static class SteamLobbyHelper
+{
+    /// <summary>
+    ///     The SteamLobbyHandler instance.
+    /// </summary>
+    public static SteamLobbyHandler? LobbyHandler { get; internal set; }
+}
