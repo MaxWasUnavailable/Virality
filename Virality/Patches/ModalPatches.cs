4c6498115ff2b1b6fa41e1127864cecde7b24108
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-04-10 17:41:50 +0200

    Fix not using ToLower in body check

--- Virality/Patches/ModalPatches.cs
+++ Virality/Patches/ModalPatches.cs
@@ -23,7 +23,7 @@ internal static class ModalPatches
         if (title.ToLower() != PhotonRoomErrorTitle)
             return;
 
-        if (!body.Contains(PhotonLobbyLimitBodyPartial))
+        if (!body.ToLower().Contains(PhotonLobbyLimitBodyPartial))
             return;
 
         var lobbyLimit = int.Parse(body.Split(':').Last());
