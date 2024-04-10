da6aa57bf22cdecfd9a1ec14d13d28444a164ae9
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-04-10 17:53:22 +0200

    Add cancel option for lobby limit override

--- Virality/Patches/ModalPatches.cs
+++ Virality/Patches/ModalPatches.cs
@@ -10,6 +10,11 @@ internal static class ModalPatches
 {
     private const string PhotonRoomErrorTitle = "failed to create photon room";
     private const string PhotonLobbyLimitBodyPartial = "max players peer room value is too big";
+    
+    private static bool IsPhotonLobbyLimitError(string title, string body)
+    {
+        return title.ToLower() == PhotonRoomErrorTitle && body.ToLower().Contains(PhotonLobbyLimitBodyPartial);
+    }
 
     /// <summary>
     ///     Prefix for the ShowError method in the Modal class.
@@ -20,10 +25,7 @@ internal static class ModalPatches
     [HarmonyPatch(nameof(Modal.ShowError))]
     private static void ShowErrorPrefix(string title, ref string body)
     {
-        if (title.ToLower() != PhotonRoomErrorTitle)
-            return;
-
-        if (!body.ToLower().Contains(PhotonLobbyLimitBodyPartial))
+        if (!IsPhotonLobbyLimitError(title, body))
             return;
 
         var lobbyLimit = int.Parse(body.Split(':').Last());
@@ -33,7 +35,31 @@ internal static class ModalPatches
 
         PhotonLobbyLimitTracker.PlayerLimit = lobbyLimit;
         
-        body += $"\n\nThe lobby player limit has been temporarily set to {lobbyLimit} for the duration of this session.";
+        body += $"\n\nThe lobby player limit will temporarily be set to {lobbyLimit} for the duration of this session.";
         body += "\nYou may use a mod such as \"Self Sufficient\" to host your own Photon server with a higher player limit.";
+    }
+    
+    /// <summary>
+    ///     Prefix for the Show method in the Modal class.
+    ///     Adds a "Cancel" option to prevent the player limit override.
+    /// </summary>
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(Modal.Show))]
+    private static void ShowPostfix(string title, ref string body, ref ModalOption[] options)
+    {
+        if (!IsPhotonLobbyLimitError(title, body))
+            return;
+        
+        var cancelOption = new ModalOption("Cancel", CancelPhotonLobbyLimitOverride);
+        options = options.Append(cancelOption).ToArray();
+    }
+    
+    /// <summary>
+    ///     Callback for the "Cancel" option in the Show method in the Modal class.
+    ///     Resets the temporary max player override.
+    /// </summary>
+    private static void CancelPhotonLobbyLimitOverride()
+    {
+        PhotonLobbyLimitTracker.PlayerLimit = null;
     }
 }
