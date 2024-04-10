45823e53e888ba1afbe234cbe94d7cba256a9881
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-04-10 20:35:40 +0200

    Remove AllowFriendJoining config

--- Virality/Virality.cs
+++ Virality/Virality.cs
@@ -16,7 +16,6 @@ public class Virality : BaseUnityPlugin
     private Harmony? Harmony { get; set; }
     internal new static ManualLogSource? Logger { get; private set; }
     internal static ConfigEntry<int>? MaxPlayers { get; private set; }
-    internal static ConfigEntry<bool>? AllowFriendJoining { get; private set; }
     internal static ConfigEntry<bool>? AllowLateJoin { get; private set; }
 
     /// <summary>
@@ -35,9 +34,6 @@ public class Virality : BaseUnityPlugin
         // Init config entries
         MaxPlayers = Config.Bind("General", "MaxPlayers", 12,
             "The maximum number of players allowed in your lobby.");
-
-        AllowFriendJoining = Config.Bind("General", "AllowFriendJoining", true,
-            "Whether or not to allow friends to join your lobby through the Steam overlay.");
 
         AllowLateJoin = Config.Bind("General", "AllowLateJoin", true,
             "Whether or not to allow players to join your lobby after the game has started.");
