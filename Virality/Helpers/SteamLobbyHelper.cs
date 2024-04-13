00dc0d2a3176959102b84806c332b32711c2cada
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-04-13 20:49:12 +0200

    Use base game static MainMenuHandler.SteamLobbyHandler

--- Virality/Helpers/SteamLobbyHelper.cs
+++ Virality/Helpers/SteamLobbyHelper.cs
@@ -13,7 +13,7 @@ public static class SteamLobbyHelper
     /// <summary>
     ///     The SteamLobbyHandler instance.
     /// </summary>
-    public static SteamLobbyHandler? LobbyHandler { get; internal set; }
+    public static SteamLobbyHandler? LobbyHandler => MainMenuHandler.SteamLobbyHandler;
 
     /// <summary>
     ///     Gets the current app ID.
