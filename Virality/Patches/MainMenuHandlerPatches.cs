796ca26f6559622577bb2c5f16dbcad4968b8aad
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-04-15 22:30:27 +0200

    Add voice app id override

--- Virality/Patches/MainMenuHandlerPatches.cs
+++ Virality/Patches/MainMenuHandlerPatches.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Reflection.Emit;
 using HarmonyLib;
+using Photon.Pun;
 using Virality.Helpers;
 
 namespace Virality.Patches;
@@ -9,6 +10,10 @@ namespace Virality.Patches;
 [HarmonyPriority(Priority.First)]
 internal static class MainMenuHandlerPatches
 {
+    /// <summary>
+    ///     Transpiler patch for the OnSteamHosted method.
+    ///     Replaces the max players value with the one from the Virality config.
+    /// </summary>
     [HarmonyTranspiler]
     [HarmonyPatch(nameof(MainMenuHandler.OnSteamHosted))]
     private static IEnumerable<CodeInstruction> OnSteamHostedTranspiler(IEnumerable<CodeInstruction> instructions)
@@ -19,5 +24,20 @@ internal static class MainMenuHandlerPatches
             .SetInstructionAndAdvance(new CodeInstruction(OpCodes.Call,
                 AccessTools.Method(typeof(LobbyHelper), nameof(LobbyHelper.GetLobbyMaxConfig))))
             .InstructionEnumeration();
+    }
+
+    /// <summary>
+    ///     Prefix patch for the ConnectToPhoton method.
+    ///     Overrides the voice server app id with the realtime server app id, in order to fix voice issues.
+    /// </summary>
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(MainMenuHandler.ConnectToPhoton))]
+    private static void ConnectToPhotonPrefix()
+    {
+        PhotonNetwork.PhotonServerSettings.AppSettings.AppIdVoice =
+            PhotonNetwork.PhotonServerSettings.AppSettings.AppIdRealtime;
+
+        Virality.Logger?.LogDebug(
+            $"Voice server app id set to realtime server app id ({PhotonNetwork.PhotonServerSettings.AppSettings.AppIdVoice})");
     }
 }
