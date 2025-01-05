a8bc1279dcf6bda977e4b9233377c4f423f4ed34
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2025-01-06 00:39:37 +0100

    Increment version

--- Virality/Virality.cs
+++ Virality/Virality.cs
@@ -19,7 +19,7 @@ public static class PluginInfo
     /// <summary>
     ///    The version of the plugin.
     /// </summary>
-    public const string PluginVersion = "1.5.2";
+    public const string PluginVersion = "1.5.3";
 }
 
 /// <summary>
