ec1cf8bceee5f09b612644ff387ef353c9ac2db9
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2025-01-05 23:12:26 +0100

    Revert "Remove voice fix from BepInEx build"
    This reverts commit aee6296975fc30d2e9d560a81e763251dfed37ec.

--- Virality/Virality.cs
+++ Virality/Virality.cs
@@ -41,8 +41,15 @@ public class Virality : BaseUnityPlugin
         AllowLateJoin = Config.Bind("General", "AllowLateJoin", true,
             "Whether or not to allow players to join your lobby after the game has started.");
 
+        EnableVoiceFix = Config.Bind("General", "EnableVoiceFix", true,
+            "Whether or not to enable the voice fix.");
+
         // Patch using Harmony
         PatchAll();
+
+        // Override voice server app id
+        if (EnableVoiceFix.Value)
+            OverrideVoiceServerAppId();
 
         // Report plugin loaded
         if (_isPatched)
@@ -73,5 +80,17 @@ public class Virality : BaseUnityPlugin
         {
             Logger?.LogError($"Failed to patch: {e}");
         }
+    }
+
+    /// <summary>
+    ///     Overrides the voice server app id with the realtime server app id, in order to fix voice issues.
+    /// </summary>
+    private static void OverrideVoiceServerAppId()
+    {
+        PhotonNetwork.PhotonServerSettings.AppSettings.AppIdVoice =
+            PhotonNetwork.PhotonServerSettings.AppSettings.AppIdRealtime;
+
+        Logger?.LogDebug(
+            $"Voice server app id set to realtime server app id ({PhotonNetwork.PhotonServerSettings.AppSettings.AppIdVoice})");
     }
 }
